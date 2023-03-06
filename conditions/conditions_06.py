e2c137474848d1d0c33b8f96efe4a3fd726e14df
Evgeny <evgenybashura@gmail.com>
2023-03-06 22:04:45 +0300

    conditions_05_06

--- conditions/conditions_06.py
+++ conditions/conditions_06.py
@@ -2,7 +2,8 @@
 # Иначе вывести надпись “DOMAIN NAME is not supported’
 
 my_mail = input('Введите вашу почту: ')
-if 'gmail.com' in my_mail:
+my_mail = my_mail.split('@')
+if 'gmail.com' in my_mail[-1]:
     print(my_mail)
 else:
     print('DOMAIN NAME is not supported')
