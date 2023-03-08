b7cbbfe85dd33b7256f6ef1c009586952ac69727
Evgeny <evgenybashura@gmail.com>
2023-03-08 23:14:56 +0300

    loops_01_try

---
+++ loops/loops_01.py
@@ -0,0 +1,12 @@
+# Выведите на экран n раз фразу "I am a programmer". Число n вводит пользователь.
+
+my_quant = int(input())
+my_count = 'I am a programmer'
+while my_quant(my_count) < my_quant:
+    print(my_count)
+    my_count = my_count + my_quant
+print('the end')
+
+# логика
+# сначала вписывается кол-во раз -> после указывается то, что будет повторяться -> затем пока кол-во повторений фразы меньше
+# кол-ва повторений, ее прописываем
