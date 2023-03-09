8edf3ae8464be4314e08f4f2fcb4bd1ad67f9e2f
Evgeny <evgenybashura@gmail.com>
2023-03-09 20:03:22 +0300

    extra_01

--- extra_zadacha/extra_01.py
+++ extra_zadacha/extra_01.py
@@ -7,13 +7,25 @@
 # 6*. Найти медиану
 # 7*. Найти верхнюю и нижнюю квартиль
 
+# arr = [1, 2, 3, 4, 4, 4, 5, 5, 2]
+# count = 0
+# sum = 0
+# while count < len(arr):
+#     sum += arr[count]
+#     count += 1
+# print(sum)
+
 arr = [1, 2, 3, 4, 4, 4, 5, 5, 2]
-count = 0
-sum = 0
-while count < len(arr):
-    sum += arr[count]
-    count += 1
-print(sum)
+arr.sort()
+print(arr)
+index_v_dkvar = 0.75 * (len(arr) - 1)
+int_index = int(index_v_dkvar)
+if index_v_dkvar % 1:
+    print(arr[int_index])
+else:
+    result = (arr[int_index] + arr[int_index + 1]) / 2
+    print(result)
+
 
 # print(sum(arr))
 #
