a965352ae252a8f1943a9e270e822fe473cc1a70
Evgeny <evgenybashura@gmail.com>
2023-03-15 17:46:12 +0300

    arr_04_05

--- extra_zadacha/arr_04.py
+++ extra_zadacha/arr_04.py
@@ -2,17 +2,14 @@
 # 4. Найти массив квадратов
 
 arr = [1, 2, 3, 4, 4, 4, 5, 5, 2]
-count = 0
-while count < len(arr):
-    count = count ** 2
-    print(count)
+result = 0
+for numbers in arr:
+    result = numbers ** 2
+    print('Квадратное значение числа равно', result)
+
+# с while не получается, сделал с for
+
 # import numpy
 # arr = [1, 2, 3, 4, 4, 4, 5, 5, 2]
 # b = list(numpy.array(arr) ** 2)
 # print(b)
-
-# arr = [1, 2, 3, 4, 4, 4, 5, 5, 2]
-# count = 0
-# while count < len(arr):
-#     arr[count] += 1
-#     print(arr[count])
--- extra_zadacha/arr_05.py
+++ extra_zadacha/arr_05.py
@@ -1,2 +1,12 @@
 # Есть список arr = [1,2,3,4,4,4,5,5,2] while
 # 5*. Найти кумулятивную сумму
+
+arr = [1, 2, 3, 4, 4, 4, 5, 5, 2]
+cum_sum = []
+j = 0
+for i in range(0, len(arr)):
+    j += arr[i]
+    cum_sum.append(j)
+print(cum_sum)
+
+# c while не понял как написать, поэтому использовал for
