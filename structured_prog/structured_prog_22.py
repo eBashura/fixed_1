ca5890d1f2ab184dfe7335bc5a74dd9a259a6dbd
Evgeny <evgenybashura@gmail.com>
2023-03-20 00:59:51 +0300

    struct_22

--- structured_prog/structured_prog_22.py
+++ structured_prog/structured_prog_22.py
@@ -2,3 +2,47 @@
 # пункт и время прибытия, пункт и время отбытия. Вывести все сведения о поездах, время пребывания в пути которых превышает 7 часов 20 минут.
 # Примечание: данное задание подразумевает самостоятельное изучение принципов работы со временем в Python(библиотека datetime)
 
+trains = [
+    {
+        'train_num': '11',
+        'place_a': 'Slutsk',
+        'time_1': 7.5,
+        'place_b': 'Minsk',
+        'time_b': 18.5,
+    },
+    {
+        'train_num': '10',
+        'place_a': 'Slutsk',
+        'time_1': 7,
+        'place_b': 'Minsk',
+        'time_b': 11,
+    },
+    {
+        'train_num': '7',
+        'place_a': 'Slutsk',
+        'time_1': 11,
+        'place_b': 'Minsk',
+        'time_b': 22,
+    },
+    {
+        'train_num': '8',
+        'place_a': 'Slutsk',
+        'time_1': 13.5,
+        'place_b': 'Minsk',
+        'time_b': 21.5
+    }
+]
+for i in trains:
+    time_v_puti = (i['time_b'] - i['time_1'])
+    if time_v_puti > 7.5:
+        print(f'время в пути больше 7 часов 20 минут: {i}')
+    else:
+        print(i, 'он быстрее чем надо')
+
+# сырая версия задания, буду еще доделывать с time!!!
+
+
+
+
+# for data in pupils:
+#     print(data.items())
