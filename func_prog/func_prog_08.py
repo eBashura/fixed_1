e9d3a91c466d0ad2553a8e58db3264c5cd6855cf
Evgeny <evgenybashura@gmail.com>
2023-03-27 22:06:04 +0300

    func_prog_08

--- func_prog/func_prog_08.py
+++ func_prog/func_prog_08.py
@@ -2,3 +2,26 @@
 # from datetime import datetime
 # time = datetime.now()
 # print(time)
+from datetime import datetime
+from functools import wraps
+from time import sleep
+
+
+def my_dec(func):
+    @wraps(func)
+    def inner(*args, **kwargs):
+        start_time = datetime.now()
+        result = func()
+        finish_time = datetime.now()
+        delta = finish_time - start_time
+        print(delta)
+        return result
+    return inner
+
+
+@my_dec
+def very_important_func():
+    sleep(2)
+    print("very_important_func")
+
+very_important_func()
