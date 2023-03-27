ddd4f2db4764497049b853b818a7109460af2cdf
Evgeny <evgenybashura@gmail.com>
2023-03-27 19:56:17 +0300

    func_prog_3-5

---
+++ func_prog/func_prog_05.py
@@ -0,0 +1,9 @@
+# Дан список чисел. Вернуть список, где каждое число переведено в строку
+# [5, 3] -> [‘5’, ‘3’]
+
+my_list = [5, 5, 6, 8, 9]
+my_list_2 = []
+for number in my_list:
+    new_number = str(number)
+    my_list_2.append(new_number)
+print(my_list_2)
