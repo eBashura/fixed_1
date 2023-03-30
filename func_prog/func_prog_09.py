325c993a4d81a544671dc9467f28ad8c8d92b483
Evgeny <evgenybashura@gmail.com>
2023-03-30 20:28:46 +0300

    func_prog_11-12

--- func_prog/func_prog_09.py
+++ func_prog/func_prog_09.py
@@ -2,8 +2,12 @@
 # где i это порядковый номер строки в списке. Использовать генератор списков.
 
 my_list_of_str = ['kafnakfnafk', 'faajfajgba', 'ajbgjabgajg', 'agnajgnag', 'jagjangja']
-a = [my_list_of_str.index(i) for i in my_list_of_str]
-print(a)
 
-#нинаю как делать
+new_list = [f'{index} - {string}' for index, string in enumerate(my_list_of_str)]
+print(new_list)
 
+#second
+new_arr = []
+for index, value in enumerate(my_list_of_str):
+    new_arr.append(f'{index} - {value}')
+print(new_arr)
--- func_prog/test.py
+++ func_prog/test.py
@@ -1,10 +1,38 @@
-from functools import reduce
+# from functools import reduce
+#
+# arr = [3, 10, 15, 10, 5, 7, 12]
+#
+# result = reduce(lambda x, y: x * y, arr)
+# print(result)
+#
+#
+# new_arr = list(map(lambda x: x / 2, arr))
+# print(new_arr)
 
-arr = [3, 10, 15, 10, 5, 7, 12]
+def my_decorator_1(func):
+    def inner(*args, **kwargs):
+        print('start 1 decorator')
+        result = func(*args, **kwargs)
+        print('finish 1 decorator')
+        return result
 
-result = reduce(lambda x, y: x * y, arr)
-print(result)
+    return inner
 
 
-new_arr = list(map(lambda x: x / 2, arr))
-print(new_arr)
+def my_decorator_2(func):
+    def inner(*args, **kwargs):
+        print('start 2 decorator')
+        result = func(*args, **kwargs)
+        print('finish 2 decorator')
+        return result
+
+    return inner
+
+
+@my_decorator_2
+@my_decorator_1 #my_func = my_decorator_2(my_decorator_1(my_func))
+def my_function():
+    print('very important my_func')
+
+
+my_function()
