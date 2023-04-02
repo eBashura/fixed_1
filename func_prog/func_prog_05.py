21d61b7bbf53c29a518d8da61062d4a0b660dfcd
Evgeny <evgenybashura@gmail.com>
2023-04-02 21:00:58 +0300

    func_prog_05_08_fix

--- func_prog/func_prog_05.py
+++ func_prog/func_prog_05.py
@@ -1,16 +1,14 @@
 # Дан список чисел. Вернуть список, где каждое число переведено в строку
 # [5, 3] -> [‘5’, ‘3’]
 
-# my_list = [5, 5, 6, 8, 9]
-# my_list_2 = []
-# for number in my_list:
-#     new_number = str(number)
-#     my_list_2.append(new_number)
-# print(my_list_2)
+my_list = [5, 5, 6, 8, 9]
+my_list_2 = []
+for number in my_list:
+    new_number = str(number)
+    my_list_2.append(new_number)
+print(my_list_2)
 
 #2 map
 my_list = [5, 5, 6, 8, 9]
-for i, elem in enumerate(my_list):
-    my_list[i] = str(elem)
-my_list_2 = list(map(str, my_list))
-print(my_list_2)
+new_list = list(map(lambda x: str(x), my_list))
+print(new_list)
