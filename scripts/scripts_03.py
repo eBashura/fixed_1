7f7f4b6e56bf06b73b748d6cb2c73360b4be0c74
Evgeny <evgenybashura@gmail.com>
2023-04-20 22:04:50 +0300

    scripts_01-03

---
+++ scripts/scripts_03.py
@@ -0,0 +1,11 @@
+# добавить описание
+
+import argparse
+import os
+
+parser = argparse.ArgumentParser()
+parser.add_argument('-dn', '--dir_name', required=True)
+args = parser.parse_args()
+print(args)
+if not os.path.isdir(args.dir_name):
+    os.mkdir(args.dir_name)
