55e86129ba0ef51a879f9b9817315b950d655bc4
Evgeny <evgenybashura@gmail.com>
2023-04-20 21:05:34 +0300

    matrix

--- oop/matrix.py
+++ oop/matrix.py
@@ -3,3 +3,19 @@
 # по-умолчанию(матрица 5 на 5 где все элементы равны нулю), копирования) , переопределить магический метод __str__ для красивого вывода.
 # Описать функции, которые принимают на вход объект класса Matrix. Функции позволяют искать максимальный элемент матрицы, минимальный, сумму всех элементов.
 # Создать в файле main.py матрицу. Воспользоваться всеми описанными функциями и методами
+from abc import ABC
+from random import randint
+
+
+class Matrix:
+    def __init__(self, data=None, n=5, m=5, random_start=0, random_finish=0):
+        if data:
+            self.__n = data.__n
+            self.__m = data.__m
+            self.__data = data.__data
+        else:
+            self.__n = data.__n
+            self.__m = data.__m
+            self.__data = [[randint(random_start, random_finish)
+                            for _ in range(m)] for _ in range(n)]
+# доделать это и 20!!!
