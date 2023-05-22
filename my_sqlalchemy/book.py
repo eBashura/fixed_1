cba23546e3b59dcd3f47443fa91e9bda8d748c6c
Evgeny <evgenybashura@gmail.com>
2023-05-22 22:08:02 +0300

    08

--- my_sqlalchemy/book.py
+++ my_sqlalchemy/book.py
@@ -2,18 +2,20 @@
 Создать базу book.db. Создать таблицу Book с помощью механизма описания таблиц sqlalchemy.
 Атрибуты: id(integer primary key), title(varchar), pages(int), author(varchar), price(float), release_year(int) """
 
-from sqlalchemy import create_engine, Table, MetaData, \
-    Column, Integer, VARCHAR, Float
+from sqlalchemy import
 
-engine = create_engine('sqlite:///book.db', echo=True)
-metadata = MetaData()
-book_table = Table('books', metadata,
-                   Column('id', Integer, primary_key=True),
-                   Column('title', VARCHAR),
-                   Column('pages', Integer),
-                   Column('author', VARCHAR),
-                   Column('price', Float),
-                   Column('release year', Integer),
-                   )
-
-metadata.create_all(engine)
+# from sqlalchemy import create_engine, Table, MetaData, \
+#     Column, Integer, VARCHAR, Float
+#
+# engine = create_engine('sqlite:///book.db', echo=True)
+# metadata = MetaData()
+# book_table = Table('books', metadata,
+#                    Column('id', Integer, primary_key=True),
+#                    Column('title', VARCHAR),
+#                    Column('pages', Integer),
+#                    Column('author', VARCHAR),
+#                    Column('price', Float),
+#                    Column('release year', Integer),
+#                    )
+#
+# metadata.create_all(engine)
