d9f34ee10977d6615ee08f6a14699cef8b50c7ce
Evgeny <evgenybashura@gmail.com>
2023-05-24 20:39:21 +0300

    sqlalchemy_homework

--- my_sqlalchemy/book.py
+++ my_sqlalchemy/book.py
@@ -2,7 +2,23 @@
 Создать базу book.db. Создать таблицу Book с помощью механизма описания таблиц sqlalchemy.
 Атрибуты: id(integer primary key), title(varchar), pages(int), author(varchar), price(float), release_year(int) """
 
-from sqlalchemy import
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+
+class Book:
+    def __init__(self, title, pages, author, price, release_year):
+        self.title = title
+        self.pages = pages
+        self.author = author
+        self.price = price
+        self.release_year = release_year
+
+
+engine = create_engine('sqlite:///book.db', echo=True)
+
+Session = sessionmaker(bind=engine)
+session1 = Session()
 
 # from sqlalchemy import create_engine, Table, MetaData, \
 #     Column, Integer, VARCHAR, Float
--- my_sqlalchemy/sqlalchemy_04.py
+++ my_sqlalchemy/sqlalchemy_04.py
@@ -7,7 +7,7 @@ from sqlalchemy import create_engine, text
 from sqlalchemy.exc import SQLAlchemyError
 
 # Создание объекта Engine
-engine = create_engine('URL: ')
+engine = create_engine('sqlite:///sa_test.db')
 
 # Получение соединения из объекта Engine
 with engine.connect() as connection:
@@ -17,26 +17,35 @@ with engine.connect() as connection:
     pages = int(input('pages: '))
     author = input('author: ')
     price = float(input('price: '))
-    release_year = int(input('release year: '))
+    release_year = int(input('release_year: '))
     try:
         # Создание объекта типа `DDL` для выполнения запроса создания таблицы
-        create_table_query = """
-            create table Book (
-                id integer primary key,
-                title varchar,
-                pages int,
-                author varchar,
-                price float,
-                release_year int
-            );
+        create_table_query = f"""
+            insert into Book (title, pages,
+                  author, price, release_year)
+                  values (:title, :pages, 
+                  :author, :price, :release_year);
         """
-        ddl = text(create_table_query)
+        ddl = text(create_table_query).bindparams(
+            title=title,
+            pages=pages,
+            author=author,
+            price=price,
+            release_year=release_year
+        )
 
         # Выполнение SQL-запроса с помощью метода `execute` на соединении
         connection.execute(ddl)
-
+        choise_trans = input(f'do you want to save book with '
+                             f'id={id}, title={title}, pages={pages}, '
+                             f'author={author}, price={price},'
+                             f' release_year={release_year} \n'
+                             f'press y to save, something else for no: ')
+        if choise_trans in 'yY':
+            transaction.commit()
+        else:
+            transaction.rollback()
         # Фиксация транзакции после успешного выполнения
-        transaction.commit()
     except SQLAlchemyError as e:
         # Обработка ошибки и откат транзакции в случае исключения
         transaction.rollback()
