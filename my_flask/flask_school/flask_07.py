e2e6dadfe62a794c63324a206739eeb5d0183999
Evgeny <evgenybashura@gmail.com>
2023-06-19 22:01:35 +0300

    flask_07-08

--- my_flask/flask_school/flask_07.py
+++ my_flask/flask_school/flask_07.py
@@ -9,7 +9,8 @@
 
 from flask import Flask, request, render_template, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
-from
+from sqlalchemy import ForeignKey
+from sqlalchemy.orm import relationship
 
 app = Flask(__name__, template_folder='templates')
 
@@ -22,16 +23,23 @@ class Group(db.Model):
     fullname = db.Column(db.String(100))
 
 
+class Student(db.Model):
+    id = db.Column('id', db.Integer, primary_key=True)
+    firstname = db.Column(db.String(100))
+    lastname = db.Column(db.String(100))
+    group_id = db.Column(db.Integer, ForeignKey(Group.id), nullable=True)
+    group = relationship('Group', foreign_keys='Student.group_id', backref='student')
+
 @app.route('/')
 def read_group():
     data = Group.query.all()
     return render_template('index.html', groups=data)
 
 
-@app.route('/add', methods=['POST', 'GET'])
+@app.route('/add_group/', methods=['POST', 'GET'])
 def add_group():
     if request.method == 'GET':
-        return render_template('form.html')
+        return render_template('add_group.html')
     else:
         group = Group(fullname=request.form['fullname'])
         db.session.add(group)
@@ -40,20 +48,22 @@ def add_group():
 
 
 @app.route('/update_group', methods=['POST', 'GET'])
-def update():
+def update_group():
     if request.method == 'GET':
-        return render_template('update.html', id=request.args.get('id'))
+        group_id = request.args.get('group_id')
+        return render_template('update_group.html', group_id=group_id)
     else:
-        group = db.session.query(Group). \
-            filter_by(id=request.args.get('id')).first()
+        group = db.session.query(Group).filter_by(id=request.args.get('group_id')).first()
         group.fullname = request.form['fullname']
         db.session.commit()
         return redirect(url_for('read_group'))
 
 
-@app.route('/delete')
-def delete():
-    db.session.query(Group).filter_by(id=request.args.get('id')).delete()
+@app.route('/delete_group/')
+def delete_group():
+    group_id = request.args.get('group_id')
+    group = db.session.query(Group).filter_by(id=group_id)
+    group.delete()
     db.session.commit()
     return redirect(url_for('read_group'))
 
