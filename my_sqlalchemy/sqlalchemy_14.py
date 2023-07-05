de692695daf50d954a09372099f1f3940cef8f4e
Evgeny <evgenybashura@gmail.com>
2023-07-05 23:47:48 +0300

    final_sql

--- my_sqlalchemy/sqlalchemy_14.py
+++ my_sqlalchemy/sqlalchemy_14.py
@@ -1,11 +1,13 @@
 """Получить все группы, где есть студенты с именем Александр."""
 
-from models import session, Group, Student
+from models import session, Group, Student, Diary
 
-data = session.query(Group).join(Student, Group.id == Student.group_id).filter(Student.firstname == 'Aleksandr').all()
-print(data)
-for i in data:
-    print(i.name)
+# data = session.query(Group, Student).join(Group, Group.id == Student.group_id).all()
+# print(data)
+# for gr, st in data:
+#     print(st.firstname, gr.name)
+
+
 
 result2 = session.query(Student, Diary).join(Diary, Student.id == Diary.student_id).filter(Diary.avg_score > 5)
 print(list(result2))
